717bc5c37843ae94ba03e329acb167686d4ac9bf
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-23 11:09:44 +0530

    file-preview page added

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,42 +1,96 @@
 
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UploadForm from './_components/UploadForm'
 import { app } from '@/firebaseConfig'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { useUser } from '@clerk/nextjs';
+import GenerateRandomString from '@/app/_utils/GenerateRandomString';
+import generateRandomString from '@/app/_utils/GenerateRandomString';
+import { useRouter } from 'next/navigation';
+
 
 const Upload = () => {
-  const [progress,setProgress] = useState(0);
+  const router=useRouter();
+
+  const {user} = useUser();
+  const [progress, setProgress] = useState(0);
+  const [uploadCompleted, setUploadCompleted] = useState(0);
+  const [fileDocId, setFileDocId] = useState(0);
   const storage = getStorage(app)
+  const db = getFirestore(app);
+
   const uploadFile = (file) => {
     const metadata = {
       contentType: file.type
     };
     const storageRef = ref(storage, 'file-upload/' + file?.name);
-    const uploadTask = uploadBytesResumable(storageRef, file, file.type);
+    const uploadTask = uploadBytesResumable(storageRef, file,metadata);
+   
     uploadTask.on('state_changed',
-    (snapshot) => {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      const cmpl = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      // console.log('Upload is ' + progress + '% done');
-      setProgress(cmpl)
+      (snapshot) => {
+        // Get task pr ogress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress);
+        console.log('Upload is ' + progress + '% done');
+        
 
-     {
-      progress==100&& getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log('File available at', downloadURL);
-      });
-    }
+        
+          progress == 100 && getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            saveInfo(file,downloadURL);
+          });
+          
+        
 
-       },)
+      },)
 
   }
+
+  const saveInfo=async(file,fileUrl)=>{
+    const docId=generateRandomString().toString();
+    await setDoc(doc(db, "uploadedFile", docId), {
+      fileName:file?.name,
+      fileSize:file?.size,
+      fileType:file.type,
+      fileUrl:fileUrl,
+      userEmail:user.primaryEmailAddress.emailAddress,
+      userName:user.fullName,
+      password:'',
+      id:docId,
+      shortUrl:process.env.NEXT_PUBLIC_BASE_URL+docId,
+
+    })
+    setFileDocId(docId);
+  }
+  useEffect(() => {
+    progress == 100 && setTimeout(() => {
+      setUploadCompleted(true);
+    }, 2000)
+  }, [progress == 100])
+  useEffect(() => {
+    uploadCompleted && setTimeout(() => {
+      setUploadCompleted(false);
+      router.push('/file-preview/'+fileDocId);
+    }, 2000)
+  }, [uploadCompleted == true])
+
   return (
     <div className='p-5 px-8 md:px-28'>
+  {uploadCompleted ? (
+    <div>YES</div>
+  ) : (
+    <div>
       <h2 className='text-[20px] text-center m-5'>Start
         <strong className='text-primary'> Uploading </strong>  File and
-        <strong className='text-primary'> Share </strong> it</h2>
+        <strong className='text-primary'> Share </strong> it
+      </h2>
       <UploadForm uploadBtnClick={(file) => uploadFile(file)} progress={progress} />
     </div>
+  )}
+</div>
+
   )
 }
 
