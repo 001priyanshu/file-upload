dcfec7a4b10c1db6fe7e7e458c5bcad7c669ec46
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-23 11:09:33 +0530

    genrating id

---
+++ app/_utils/GenerateRandomString.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const generateRandomString = () => {
+   const characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+   let result="";
+   for(let i=0;i<6;i++){
+    result+=characters.charAt(Math.floor(Math.random()*characters.length));
+   }
+   return result;
+}
+
+export default generateRandomString
